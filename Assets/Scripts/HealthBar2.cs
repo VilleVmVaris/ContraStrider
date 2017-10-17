15e517cf748a03667be0a214800ca6f1a52dcbc7
OPISTO\katja.mäkelä <katja.makela@gmail.com>
2017-10-17 14:26:49 +0300

    Health meter version 2.0

---
+++ Assets/Scripts/HealthBar2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar2 : MonoBehaviour {
+    float healthBarFull;
+    public float maxHealth = 50f;
+    float health;
+    float healthBarHeight;
+
+    void Awake() {
+        healthBarHeight = GetComponent<RectTransform>().rect.height;
+        healthBarFull = /*GetComponent<RectTransform>().rect.width*/ 500;
+        health = /*healthBarFull*/0;
+        SetHealthBar(0);
+    }
+
+    void Update() {
+        //***Testing***
+        if (Input.GetKeyDown(KeyCode.Delete)) {
+            SetHealthBar(1);
+        }
+        //***Testing***
+        if (health <= 0) {
+            print("Game Over!!!");
+        }
+
+    }
+    public void SetHealthBar(float damage) {
+        // health -= damage;
+        health += (healthBarFull / maxHealth) * damage;
+        //transform.localScale = new Vector3(health , 1, 1);
+        GetComponent<RectTransform>().sizeDelta = new Vector2(health, healthBarHeight);
+    }
+}
