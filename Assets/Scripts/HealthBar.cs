5fa7cbd5dc196725e1e106d07341d45de174f745
OPISTO\katja.mäkelä <katja.makela@gmail.com>
2017-10-18 11:17:59 +0300

    Player health meter. Ready for check. Testin with "Delete".

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar2 : MonoBehaviour {
+public class HealthBar : MonoBehaviour {
     float healthBarFull;
-    public float maxHealth = 50f;
+    public float maxHealth;
     float health;
     float healthBarHeight;
 
     void Awake() {
         healthBarHeight = GetComponent<RectTransform>().rect.height;
-        healthBarFull = /*GetComponent<RectTransform>().rect.width*/ 500;
-        health = /*healthBarFull*/0;
-        SetHealthBar(0);
+        healthBarFull = GetComponent<RectTransform>().rect.width;
+        health = 0;
+        SetHealthBar(health);
     }
 
     void Update() {
@@ -28,10 +28,8 @@ public class HealthBar2 : MonoBehaviour {
 
     }
     public void SetHealthBar(float damage) {
-        // health -= damage;
         if (health < healthBarFull) { 
         health += (healthBarFull / maxHealth) * damage;
-        //transform.localScale = new Vector3(health , 1, 1);
         GetComponent<RectTransform>().sizeDelta = new Vector2(health, healthBarHeight);
         }
     }
