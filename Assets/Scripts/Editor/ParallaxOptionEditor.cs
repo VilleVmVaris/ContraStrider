a3965ce7383d7ef1d340d0b0e632ed17f3462d32
Antti Harju <antti.harju@saunalahti.fi>
2017-10-23 13:46:30 +0300

    Parallax and camera movement options

---
+++ Assets/Scripts/Editor/ParallaxOptionEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ParallaxOptions))]
+public class ParallaxOptionEditor : Editor {
+
+	ParallaxOptions parallaxOptions;
+
+	// Use this for initialization
+	void Awake () {
+		parallaxOptions = target as ParallaxOptions;
+	}
+	
+	public override void OnInspectorGUI () {
+		base.OnInspectorGUI();
+		if (GUILayout.Button("Save Position")) {
+			parallaxOptions.SaveCameraPosition();
+			EditorUtility.SetDirty(parallaxOptions);
+		}
+		if (GUILayout.Button("Load Position")) {
+			parallaxOptions.LoadCameraPosition();
+		}
+	}
+}
--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -11,19 +11,24 @@ public class ParallaxLayer : MonoBehaviour {
 
 	Transform cameraTransform;
 	Vector3 previousCameraPosition;
-
-	// TODO: options
+	ParallaxOptions options;
 
 	// Use this for initialization
 	void Start () {
 		cameraTransform = Camera.main.transform;
 		previousCameraPosition = cameraTransform.position;
+		options = Camera.main.GetComponent<ParallaxOptions>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (options) {
+			if (!options.moveParallaxLayers) {
+				return;
+			}
+		}
 		var delta = cameraTransform.position - previousCameraPosition;
 		transform.position += Vector3.Scale(delta, new Vector3(speedX, speedY));
-		previousCameraPosition = cameraTransform.position;
+		previousCameraPosition = cameraTransform.position;	
 	}
 }
