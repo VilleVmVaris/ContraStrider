3daf62c6b22ef0bb8a7ae78dfb819aebe61fa15a
Antti Harju <antti.harju@saunalahti.fi>
2017-11-17 18:01:31 +0200

    Shadow effect component

---
+++ Assets/Scripts/CharacterShadow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterShadow : MonoBehaviour {
+
+	public float shadowDistance;
+
+	SpriteRenderer sr;
+	Vector3 groundPosition;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer>();
+		groundPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, shadowDistance, Helpers.ObstacleLayerMask);
+		if (hit.collider != null) {
+			sr.enabled = true;
+
+// 			//TODO: Fade and position shadow
+//			Color color = sr.material.color;
+//			color.a = ??
+//			sr.material.color = color;
+		} else {
+			sr.enabled = false;
+		}
+	}
+}
