e779eb55ddc1d07e9f47d006b092988985560133
Antti Harju <antti.harju@saunalahti.fi>
2017-11-20 13:14:49 +0200

    Fix shadow script raycast

--- Assets/Scripts/CharacterShadow.cs
+++ Assets/Scripts/CharacterShadow.cs
@@ -7,21 +7,21 @@ public class CharacterShadow : MonoBehaviour {
 	public float shadowDistance;
 
 	SpriteRenderer sr;
-	Vector3 groundPosition;
+	Transform player;
 
 	// Use this for initialization
 	void Start () {
 		sr = GetComponent<SpriteRenderer>();
-		groundPosition = transform.localPosition;
+		player = transform.root;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, shadowDistance, Helpers.ObstacleLayerMask);
+		RaycastHit2D hit = Physics2D.Raycast(player.position, Vector2.down, shadowDistance, Helpers.ObstacleLayerMask);
 		if (hit.collider != null) {
 			sr.enabled = true;
-
-// 			//TODO: Fade and position shadow
+			transform.position = hit.point;
+// 			//TODO: Fade and/or scale?
 //			Color color = sr.material.color;
 //			color.a = ??
 //			sr.material.color = color;
