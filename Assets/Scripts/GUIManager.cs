e8000ac2ae053f3df7008d60800a29930afde192
Antti Harju <antti.harju@saunalahti.fi>
2017-11-20 17:16:27 +0200

    Game GUI prefab

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour {
+
+	// TODO: GUI logic stuff
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
