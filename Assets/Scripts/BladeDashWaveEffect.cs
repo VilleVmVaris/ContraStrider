a62ec3a89809d6f6b728a585277810a53f873851
Antti Harju <antti.harju@saunalahti.fi>
2017-11-27 12:52:39 +0200

    First draft blade dash effect

---
+++ Assets/Scripts/BladeDashWaveEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeDashWaveEffect : MonoBehaviour {
+
+	public float speed;
+
+	static readonly Vector3 START_SIZE = new Vector3(.1f, .1f, 1f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// TODO: play with noise texture offset?
+
+		var scale = transform.localScale;
+		var newScale = scale.x + speed * Time.deltaTime;
+		scale = new Vector3(newScale, newScale);
+		if (scale.magnitude > 6f) {
+			scale = START_SIZE;
+		}
+		transform.localScale = scale;
+	}
+}
