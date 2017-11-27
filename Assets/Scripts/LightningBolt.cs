ec0e6bbb0e3bfb78af4427a81a91a62fe708f0ec
Antti Harju <antti.harju@saunalahti.fi>
2017-11-27 16:53:48 +0200

    Components for lightning bolt effects

---
+++ Assets/Scripts/LightningBolt.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningBolt : MonoBehaviour {
+
+	public List<GameObject> activeSegments;
+	public List<GameObject> inactiveSegments;
+
+	public GameObject segmentPrefab;
+
+	public float alpha;
+	public float fadeOutRate;
+	public Color tint;
+
+	public Vector2 Start {
+		get { 
+			return activeSegments[0].GetComponent<LightningSegment>().start;
+		}
+	}
+
+	public Vector2 End {
+		get { 
+			return activeSegments[activeSegments.Count - 1].GetComponent<LightningSegment>().end;
+		}
+	}
+
+	public bool IsFadeOutComplete {
+		get { 
+			return alpha <= 0;
+		}
+	}
+
+	public void Init(int maxSegments) {
+		// Pool segments into lists
+		activeSegments = new List<GameObject>(maxSegments);
+		inactiveSegments = new List<GameObject>(maxSegments);
+	
+		for (int i = 0; i < maxSegments; i++) {
+			GameObject segment = Instantiate(segmentPrefab, transform);
+			segment.SetActive(false);
+			inactiveSegments.Add(segment);
+		}
+	}
+
+	public void FireBolt(Vector2 source, Vector2 destination, Color color, float thickness) {
+		tint = color;
+		alpha = 1.5f;
+		fadeOutRate = 0.03f;
+
+		// Set minimum size
+		if (Vector2.Distance(destination, source) <= 0) {
+			Vector2 adjust = Random.insideUnitCircle;
+			if (adjust.magnitude <= 0) {
+				adjust.x += .1f;
+			}
+			destination += adjust;
+		}
+
+		var slope = destination - source;
+		var normal = (new Vector2(slope.y, -slope.x)).normalized;
+		var distance = slope.magnitude;
+
+		List<float> positions = new List<float>();
+		positions.Add(0);
+
+		// Generate random positions
+		for (int i = 0; i < distance / 4; i++) {
+			positions.Add(Random.Range(.25f, .75f));
+		}
+
+		positions.Sort();
+
+		var sway = 80;
+		var jaggies = 1 / sway;
+		var spread = 1f;
+
+		// Start at source
+		var previous = source;
+		var previousDisplacement = 0f;
+
+		for (int i = 0; i < positions.Count; i++) {
+			// Stop at pool size
+			if (inactiveSegments.Count <= 0) {
+				break;
+			}
+
+			var pos = positions[i];
+
+			// Perpendicular variation for close positions, prevents sharp angles
+			var scale = (distance * jaggies) * (pos - positions[i - 1]);
+			// Points to near middle of the bolt
+			var envelope = pos > .95f ? 20 * (1 - pos) : spread;
+
+			float displacement = Random.Range(-sway, sway);
+			displacement -= (displacement - previousDisplacement) * (1f - scale);
+			displacement *= envelope;
+
+			// End point
+			var point = source + (pos * slope) + (displacement * normal);
+
+			activateLineSegment(previous, point, thickness);
+			previous = point;
+			previousDisplacement = displacement;
+		}
+	}
+
+	void activateLineSegment(Vector2 start, Vector2 end, float thickness) {
+		var inactiveCount = inactiveSegments.Count;
+		if (inactiveCount <= 0) {
+			return;
+		}
+		var segment = inactiveSegments[inactiveCount - 1];
+		segment.SetActive(true);
+		var component = segment.GetComponent<LightningSegment>();
+		component.SetColor(Color.white);
+		component.start = start;
+		component.end = end;
+		component.thickness = thickness;
+		inactiveSegments.RemoveAt(inactiveCount - 1);
+		activeSegments.Add(segment);
+	}
+
+	public void DeactívateSegments() {
+		for (int i = activeSegments.Count - 1; i >= 0 ; i--) {
+			var segment = activeSegments[i];
+			segment.SetActive(false);
+			activeSegments.RemoveAt(i);
+			inactiveSegments.Add(segment);
+		}
+	}
+
+	public void Draw() {
+		if (alpha <= 0) {
+			return;
+		}
+
+		foreach (var segment in activeSegments) {
+			var component = segment.GetComponent<LightningSegment>();
+			component.SetColor(tint * (alpha * 0.6f));
+			component.Draw();
+		}
+	}
+
+	public void UpdateComponent() {
+		alpha -= fadeOutRate;
+	}
+}
---
+++ Assets/Scripts/LightningSegment.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningSegment : MonoBehaviour {
+
+	public Vector2 start;
+	public Vector2 end;
+	public float thickness;
+
+	public GameObject startCap;
+	public GameObject body;
+	public GameObject endCap;
+
+
+	public LightningSegment(Vector2 start, Vector2 end, float thickness) {
+		this.start = start;
+		this.end = end;
+		this.thickness = thickness;
+	}
+
+	public void SetColor(Color color) {
+		startCap.GetComponent<SpriteRenderer>().color = color;
+		body.GetComponent<SpriteRenderer>().color = color;
+		endCap.GetComponent<SpriteRenderer>().color = color;
+	}
+
+	public void Draw() {
+		var direction = end - start;
+		var rotation = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+
+		// Scale to lenght and distance
+		body.transform.localScale = new Vector3(
+			100 * direction.magnitude / body.GetComponent<SpriteRenderer>().sprite.rect.width,
+			thickness, 
+			body.transform.localScale.z);
+
+		startCap.transform.localScale = new Vector3(
+			startCap.transform.localScale.x,
+			thickness, 
+			body.transform.localScale.z);
+
+		endCap.transform.localScale = new Vector3(
+			startCap.transform.localScale.x,
+			thickness, 
+			body.transform.localScale.z);
+	
+		// Rotate
+		body.transform.rotation = Quaternion.Euler(0, 0, rotation);
+		startCap.transform.rotation = Quaternion.Euler(0, 0, rotation);
+		endCap.transform.rotation = Quaternion.Euler(0, 0, rotation);
+
+		// Convert rotation to radians for Cos/Sin later
+		rotation *= Mathf.Deg2Rad;
+
+		// Store adjustment value
+		var bodyWorldAdjustment = body.transform.localScale.x * body.GetComponent<SpriteRenderer>().sprite.rect.width / 2f;
+		var startWorldAdjustment = startCap.transform.localScale.x * startCap.GetComponent<SpriteRenderer>().sprite.rect.width / 2f;
+		var endWorldAdjustment = endCap.transform.localScale.x * endCap.GetComponent<SpriteRenderer>().sprite.rect.width / 2f;
+	
+		// Adjust child positions
+		body.transform.position += new Vector3(
+			.01f * Mathf.Cos(rotation) * bodyWorldAdjustment,
+			.01f * Mathf.Sin(rotation) * bodyWorldAdjustment,
+			0);
+
+		startCap.transform.position += new Vector3(
+			.01f * Mathf.Cos(rotation) * startWorldAdjustment,
+			.01f * Mathf.Sin(rotation) * startWorldAdjustment,
+			0);
+
+		endCap.transform.position += new Vector3(
+			.01f * Mathf.Cos(rotation) * bodyWorldAdjustment * 2f,
+			.01f * Mathf.Sin(rotation) * bodyWorldAdjustment * 2f,
+			0);
+		endCap.transform.position += new Vector3(
+			.01f * Mathf.Cos(rotation) * endWorldAdjustment,
+			.01f * Mathf.Sin(rotation) * endWorldAdjustment,
+			0);
+	}
+}
