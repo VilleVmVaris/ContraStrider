ede700eef2da9d3abcd3e6ee2dba61d692794e21
OPISTO\ville.varis <ville.vm.varis@gmail.com>
2017-12-06 16:56:51 +0200

    Game loads credits scene after boss dies. Added credits scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,15 @@ public class GameManager : MonoBehaviour {
 	Serializer serializer;
 	GUIManager gui;
 	Player player;
+    TimerManager timer;
 
 	// Use this for initialization
 	void Start() {
 		serializer = GetComponentInChildren<Serializer>();
 		gui = GameObject.Find("GameCanvas").GetComponent<GUIManager>();
 		player = GameObject.Find("Player").GetComponent<Player>();
+        timer = GetComponent<TimerManager>();
+
 	}
 	
 	// Update is called once per frame
@@ -23,6 +26,7 @@ public class GameManager : MonoBehaviour {
 
 	public void EndGame() {
 		gui.FadeToBlack(5f);
+        timer.Once(ShowCredits, 10);
 		// wait and load credits scene?
 	}
 
@@ -30,4 +34,9 @@ public class GameManager : MonoBehaviour {
 		serializer.LoadCheckPoint();
 		player.Restart();
 	}
+
+    void ShowCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
